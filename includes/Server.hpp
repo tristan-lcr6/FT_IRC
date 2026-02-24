ab2453d85f62d4643fbe46c9bcbb4572fa9ca786
Tristan Lecuyer <tlecuyer@f6r11s4.clusters.42paris.fr>
2026-02-24 17:06:08 +0100

    includes

--- includes/Server.hpp
+++ includes/Server.hpp
@@ -3,15 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   Server.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jferrand <jferrand@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tlecuyer <tlecuyer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/24 12:22:56 by jferrand          #+#    #+#             */
-/*   Updated: 2026/02/24 12:23:27 by jferrand         ###   ########.fr       */
+/*   Updated: 2026/02/24 17:00:55 by tlecuyer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SERVER_HPP
 # define SERVER_HPP
+
+#include <iostream>
+#include <vector> //-> for vector
+#include <sys/socket.h> //-> for socket()
+#include <sys/types.h> //-> for socket()
+#include <netinet/in.h> //-> for sockaddr_in
+#include <fcntl.h> //-> for fcntl()
+#include <unistd.h> //-> for close()
+#include <arpa/inet.h> //-> for inet_ntoa()
+#include <poll.h> //-> for poll()
+#include <csignal> //-> for signal()
+
+#define BLACK  "\033[90m"
+#define RED	"\033[91m"
+#define GREEN	"\033[92m"
+#define YELLOW	"\033[93m"
+#define BLUE	"\033[94m"
+#define PURPLE	"\033[95m"
+#define Cyan	"\033[96m"
+#define END	"\033[0m"
+
+
 
 
 
