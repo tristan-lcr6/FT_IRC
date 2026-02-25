c61e41a15f9009083a1230a0ca99f01bb6c73808
tristan-lcr6 <tristan.l0244@gmail.com>
2026-02-25 15:30:17 +0100

    main init

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tlecuyer <tlecuyer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/24 12:22:35 by jferrand          #+#    #+#             */
-/*   Updated: 2026/02/25 12:50:07 by tlecuyer         ###   ########.fr       */
+/*   Updated: 2026/02/25 15:14:08 by tlecuyer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,26 @@
 
 int	main(int argc, char **argv)
 {
+	Server	ser;
+
 	if (argc != 3)
 		return ((std::cout << "Irc error : Bad arguments " << std::endl), 1);
-	// Server ser;
-	// std::cout << "---- SERVER ----" << std::endl;
-	// try{
-	// 	signal(SIGINT, Server::SignalHandler); //-> catch the signal (ctrl + c)
-	// 	signal(SIGQUIT, Server::SignalHandler); //-> catch the signal (ctrl + \)
-	// 	ser.ServerInit(); //-> initialize the server
-	// }
-	// catch(const std::exception& e){
-	// 	ser.CloseFds(); //-> close the file descriptors
-	// 	std::cerr << e.what() << std::endl;
-	// }
-	// std::cout << "The Server Closed!" << std::endl;
-
+	if (atoi(argv[2]) < 1024 || atoi(argv[2]) > 65535 )
+		return ((std::cout << "Irc error : Bad arguments " << std::endl), 1);
+	std::cout << "---- SERVER ----" << std::endl;
+	ser.setPassword(atoi(argv[2]));
+	ser.setPort(argv[1]);
+	try
+	{
+		signal(SIGINT, Server::SignalHandler);
+		signal(SIGQUIT, Server::SignalHandler);
+		ser.ServerInit();
+	}
+	catch (const std::exception &e)
+	{
+		ser.CloseFds();
+		std::cerr << e.what() << std::endl;
+	}
+	std::cout << "The Server Closed!" << std::endl;
 	return (0);
 }
